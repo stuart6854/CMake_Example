da62e315ac49f23415520d7a1c6e3aac07951c7b
Stuart Millman <stu.millman15@gmail.com>
2021-01-07 14:22:25 +0000

    Backup

--- app_proj/src/main.cpp
+++ app_proj/src/main.cpp
@@ -7,28 +7,26 @@
 #include <lib/Vec3.h>
 #include <lib2/World.h>
 
-int main(int, char **)
-{
-    std::cout << "Hello, world!\n";
+int main(int, char **) {
+  std::cout << "Hello, world!\n";
 
-    std::vector<int> v = {5, 2, 10, 89};
+  std::vector<int> v = {5, 2, 10, 89};
 
-    for (auto x : v)
-    {
-        std::cout << x << "\n";
-    }
+  for (auto x : v) {
+    std::cout << x << "\n";
+  }
 
-    Hello hello;
-    hello.SayHello();
+  Hello hello;
+  hello.SayHello();
 
-    Vec3 a;
-    a.x = 2;
-    a.y = 1;
-    a.z = 3;
+  Vec3 a;
+  a.x = 2;
+  a.y = 1;
+  a.z = 3;
 
-    std::cout << a.x << ", " << a.y << ", " << a.z << "\n";
+  std::cout << a.x << ", " << a.y << ", " << a.z << "\n";
 
-    World::say_world();
+  World::say_world();
 
-    return 0;
+  return 0;
 }
